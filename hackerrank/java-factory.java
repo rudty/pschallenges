de611b9a56e7204f17ebfa53eb4ae110370f8fba
rudty <rudtyz@gmail.com>
2020-04-08 20:41:59 +0900

    2020-04-08

---
+++ hackerrank/java-factory.java
@@ -0,0 +1,7 @@
+        if ("cake".equals(order)) {
+            return new Cake();
+        }
+        if ("pizza".equals(order)) {
+            return new Pizza();
+        }
+        return null;
